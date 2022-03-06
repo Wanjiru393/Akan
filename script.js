3db1ea499937b2b0b775c19a5241a56b1aaaabac
Wanjiru393 <carolciru393@gmail.com>
2022-03-06 16:22:59 +0300

    Added data to script.js

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 //Put values in specific containers(variables)
-var year=document.getElementById("year").value;
-var month=document.getElementById("month").value;
-var day=document.getElementById("day").value; //change strings to numbers.
+function getAkanName (){
+    var year=document.getElementById("year").value;
+    var month=document.getElementById("month").value;
+    var day=document.getElementById("month").value;
+//change strings to numbers.
+    var yy=parseInt(year);
+    var mm=parseInt(month);
+    var dd=parseInt(day);
+    //adding data
+    var days=[
+        "Sunday","Monday","Tuesday","Wednesday",
+        "Thursday","Friday","Saturday"
+    ]
+    var femaleName=[
+        "Akosua","Adwoa","Abenaa","Akua",
+        "Yaa","Afua","Ama"
+    ]
+    var maleName=[
+        "Kwasi","Kwadwo","Kwabena","Kwaku",
+        "Yaw","Kofi","Kwame"
+    ]
+
+
+
+
+
+
+
+}
+     
+
+
 
 
 
