51830707139810eb846d9489a3006cb7dbfebfc6
Wanjiru393 <carolciru393@gmail.com>
2022-03-06 15:41:18 +0300

    Added variables

---
+++ script.js
@@ -0,0 +1,8 @@
+//Put values in specific containers(variables)
+var year=document.getElementById("year").value;
+var month=document.getElementById("month").value;
+var day=document.getElementById("day").value; //change strings to numbers.
+
+
+
+
