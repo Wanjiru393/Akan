4aa5cabe9c1042a837925ce069d9b67f7aa88fa6
Wanjiru393 <carolciru393@gmail.com>
2022-03-06 23:19:10 +0300

    Added more JavaScript

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 let submit = document.getElementById("submit");
-male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"],
-female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"],
+let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
 function showAkanName(event) {
     event.preventDefault()
     let date = document.getElementById("date").value;
-    if (date == "") return alert("Please input date");
-    let d = new Date(date);
-
+    if (!date) {
+        alert("Please input date")
+        return
+    };
     let gender = document.getElementById("gender");
     let optionValue = gender.options[gender.selectedIndex].value;
-
-    alert(optionValue)
+    if (optionValue == "none") {
+        alert("Please select gender")
+        return
+    };
+    let d = new Date(date);
+    let day = d.getDay();
+    if (optionValue == "male") {
+    //Loop through male array
+    for (let i = 0; i < male.length; i++) {
+      if (parseInt(day) === i) {
+        alert(male[i]);
+      }
+    }
+    } else {
+    //Loop through female array
+    for (let i = 0; i < female.length; i++) {
+    if (parseInt(day) === i) {
+      alert(female[i]);
+    }
+    }
+    }
 }
 
 
